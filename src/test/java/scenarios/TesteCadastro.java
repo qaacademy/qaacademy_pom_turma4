ce0c00fee84acbbc0ee29f591bc6004c1a20643a
qaacademy <qaacademy@gmail.com>
2022-05-21 15:35:42 -0300

    Primeiro Commit POM

---
+++ src/test/java/scenarios/TesteCadastro.java
@@ -0,0 +1,43 @@
+package scenarios;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+
+import java.util.concurrent.TimeUnit;
+
+public class TesteCadastro {
+    WebDriver driver;
+    HomePage homePage;
+
+    @Before
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        homePage = new HomePage(driver);
+
+        driver.get("http://localhost:3000");
+    }
+
+    @Test
+    public void testeCadastro() {
+
+        homePage.clicarRegistrar();
+        homePage.preencherEmail();
+        homePage.preencherNome();
+        homePage.preencherSenha();
+        homePage.preencherConfirmacaoSenha();
+        homePage.clicarCriarComSaldo();
+        homePage.clicarCadastrar();
+        homePage.clicarFecharContaCriada();
+    }
+
+    @After
+    public void finaliza() {
+        driver.quit();
+    }
+}
